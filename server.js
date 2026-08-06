72bc00f8d91a49fd0d3500f397843bc1804fb7e7
Bhuvaneswaran S <bhuvanes2701@gmail.com>
2026-08-06 07:47:39 +0000

    Test CI/CD

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.get("/", (req, res) => {
-    res.send("Welcome to ElectroMart - CI/CD Working");
+    res.send("ElectroMart CI/CD Successfully Working!");
 });
 
 const PORT = 3000;
