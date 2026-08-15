5d2e76f9099b14071e6e857db7d9d86eba0bab1f
Bhuvaneswaran S <bhuvanes2701@gmail.com>
2026-08-15 08:31:08 +0000

    Use environment variables for RDS connection

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "electromart-db.c7su62cgo0bo.ap-south-1.rds.amazonaws.com",
-    user: "admin",
-    password: "RkQ5fPAe72d224W",
-    database: "Electromart",
+    host: process.env.DB_"electromart-db.c7su62cgo0bo.ap-south-1.rds.amazonaws.com",
+    user: process.env.DB_"admin",
+    password: process.env.DB_"RkQ5fPAe72d224W",
+    database: process.env.DB_"Electromart",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
